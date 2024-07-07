07a254d808ea474680d73dbd29a36848a8339cef
Harutyun Matosoghlyan <harutyunmatosoghlyan@gmail.com>
2024-07-07 14:06:21 +0400

    First Homework

---
+++ src/homeworks/homework1/ForIfExamples.java
@@ -0,0 +1,29 @@
+package homeworks.homework1;
+
+public class ForIfExamples {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        int sum = 0;
+        for (int i = 0; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+        for (int i = 50; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < 20; i = i + 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        int even = 0;
+        for (int i = 0; i < 100; i = i + 2) {
+//            System.out.println(i);
+            even++;
+        }
+        System.out.print(even);
+    }
+}
