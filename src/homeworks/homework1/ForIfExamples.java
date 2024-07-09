7b83f24877bd48401566b27855cdac097370f8a5
Harutyun Matosoghlyan <harutyunmatosoghlyan@gmail.com>
2024-07-09 15:45:30 +0400

    First Homework. I updated the last cycle, now it starts not from 0, but from 1

--- src/homeworks/homework1/ForIfExamples.java
+++ src/homeworks/homework1/ForIfExamples.java
@@ -20,7 +20,7 @@ public class ForIfExamples {
         }
         System.out.println();
         int even = 0;
-        for (int i = 2; i < 100; i = i + 2) {
+        for (int i = 1; i < 100; i = i + 2) {
             even++;
         }
         System.out.print(even);
