d32a2c648ca10518ebe27c2e6bad7fff95a266b0
Harutyun Matosoghlyan <harutyunmatosoghlyan@gmail.com>
2024-07-12 23:08:10 +0400

    Homework 2,FigurePainter

---
+++ src/homeworks/homework1/FigurePainter.java
@@ -0,0 +1,63 @@
+package homeworks.homework1;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+//         1
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+//         2
+        System.out.println();
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+//         3
+        System.out.println();
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+//         4
+        System.out.println();
+        for (int i = 5; i > 0; i--) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+//         5
+        System.out.println();
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
